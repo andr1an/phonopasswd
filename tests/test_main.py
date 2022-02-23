61ac95e2d723b318a8069a30666eab4bf1978040
andr1an <halavshik@gmail.com>
2022-02-23 13:08:18 +0300

    Update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
-from phonopasswd import generate_password
+from phonopasswd import PhonoPasswd
 import re
 
 
 def test_generate_password():
     re_passwd = re.compile(r"^[-_\da-z]{9,}$", re.IGNORECASE)
+    passwordgen = PhonoPasswd(language="en")
     for i in range(10):
-        assert re_passwd.match(generate_password())
+        assert re_passwd.match(passwordgen.generate_password())
